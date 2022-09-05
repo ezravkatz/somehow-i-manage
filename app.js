58e25f15f112e24a71f14a72a74b7fd2015748ec
ezravkatz <ezravkatz@gmail.com>
2022-09-05 16:54:08 -0400

    add user page render

--- app.js
+++ app.js
@@ -1,19 +1,11 @@
 const express = require("express");
 const express_handlebars = require("express-handlebars");
-const bodyParser = require("body-parser");
 const mysql = require("mysql2");
 
 require("dotenv").config();
 
 const app = express();
 const port = process.env.port || 3001;
-
-//parse middleware
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-//parse app/json
-app.use(bodyParser.json());
 
 //static files
 app.use(express.static("public"));
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,19 +1,65 @@
 const express = require('express');
-const router = express.Router()
-const userController = require('../controllers/userController');
+const app = express()
+// const userController = require('../controllers/userController');
+// const pool = require('../controllers/userController')
+const router = express.Router();
 
-//crud
-
-router.get('/', function (req, res) {
+//render home page 
+app.get('/',(req, res) => {
   res.render('home', {});
 });
-router.get('/', function(req, res) {
-  userController.view(rows)});
-router.post('/', function (req, res) {
-  userController.find(rows)});
-// router.post('/:id', UserController.delete)
-// router.post('/add-user', UserController.add);
-// router.get('/edit-user', UserController.edit);
-// router.post('/edituser/:id', UserController.update);
 
-module.exports = router;
+//render add user form
+app.get('/add-user', (req, res) => {
+  res.render('add-user', {});
+});
+
+//render edit user
+app.get('/edit-user', (req, res) => {
+  res.render('edit-user', {});
+});
+
+// app.get('', (req, res) => {
+//   pool.getConnection((err, connection) => {
+//     if(err) throw err
+//     console.log('Connected as ID' + connection.threadID)
+//     connection.query(`SELECT * from user`, (err, rows) => {
+//       connection.release() //release connection when finished
+
+//       if(!err) {
+//         res.send(rows)
+//       } else {
+//         console.log(err)
+//       }
+
+//       console.log('The data from the usermanagement table is: \n', rows)
+//     })
+//   })
+// });
+
+// router.get('/userController', function(req, res) {
+//   let sql = 'SELECT * FROM user';
+//   pool.query(sql, function (err, data) {
+//     if (err) throw err;
+//     res.render('user', {});
+//   });;
+
+// });
+
+// router.get('/', function (req, res) {
+//   res.render('home', {});
+// });
+// router.get('/', function(req, res) {
+//   userController.view()});
+// router.post('/', function (req, res) {
+//   userController.find()});
+// router.post('/:id', function(req, res) {
+//   userController.delete()});
+// router.post('/add-user', function (req, res) {
+//   userController.add()});
+// router.get('/edit-user', function (req, res) {
+//    userController.edit()});
+// router.post('/edituser/:id', function (res, req) {
+//    userController.update()});
+
+module.exports = app;
